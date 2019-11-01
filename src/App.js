895df8209bb7f2656fbf124177e732c1cd9e8f2b
Alex <alexandr.dubiaga@gmail.com>
2019-11-01 09:28:16 +0200

    created ava+descr My posts postt on content page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,36 @@ const App = () => {
             <header className="header">
                 <img src="https://mirpozitiva.ru/uploads/posts/2016-08/medium/1472042903_31.jpg"/>
             </header>
-            <nav  className="nav">
+            <nav className="nav">
                 <div>
-                    Profile
+                    <a href="#">Profile</a>
                 </div>
                 <div>
-                    Messages
+                    <a href="#"> Messages</a>
+                </div>
+                <div>
+                    <a href="#"> News</a>
+                </div>
+                <div>
+                    <a href="#">Music</a>
+                </div>
+                <div>
+                    <a href="#">Settings</a>
                 </div>
             </nav>
-            <div  className="content">
-                Main Content
+            <div className="content">
+                <div>
+                    <img src="https://klike.net/uploads/posts/2019-07/1562069947_1.jpg"/>
+                </div>
+                <div>
+                    ava+description
+                </div>
+                <div>
+                    My Posts
+                    <div>
+                        New Post
+                    </div>
+                </div>
             </div>
         </div>
     );
