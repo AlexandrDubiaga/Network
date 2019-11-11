7dd4e3fdbb6f51773aa5098cb57bedf29d651701
Alex <alexandr.dubiaga@gmail.com>
2019-11-11 10:00:10 +0200

    add styles to Profile component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,14 +5,20 @@ import Post from "./Posts/Post";
 
 const MyPosts = () => {
     return (
-        <div>
-            My Posts
+        <div className={style.postBlock}>
+            <h3>My Posts</h3>
             <div>
-               <textarea></textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
-            <Post message="Hi im new Post"/>
-            <Post message="Its my first Post"/>
+            <div className={style.posts}>
+                <Post message="Hi im new Post"/>
+                <Post message="Its my first Post"/>
+            </div>
         </div>
     );
 }
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,11 +3,10 @@ import style from './ProfileInfo.module.css';
 const ProfileInfo = () => {
     return (
         <div>
-
             <div>
                 <img src="https://klike.net/uploads/posts/2019-07/1562069947_1.jpg"/>
             </div>
-            <div>
+            <div className={style.descriptionBlock}>
                 ava+description
             </div>
         </div>
